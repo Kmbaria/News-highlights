c84e7ca3e616568e51fe8f9f4d112621ce0f0229
Kmbariah <Kmbaria@github.com>
2020-04-30 13:53:42 +0300

    html

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/abdirahman/Documents/news-highlighter/virtual"
+set -gx VIRTUAL_ENV "/home/user/news-highlights/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
